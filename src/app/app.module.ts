7958d20dab07b467af7cc5bc9e4c35d027c1c747
Varun Thakur <001vrn@Varuns-MacBook-Pro.local>
2018-04-02 20:55:45 +0530

    stable changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SeekComponent } from './seek/seek.component';
 import { DetailsComponent } from './details/details.component';
 import { MainComponent } from './main/main.component';
+
 import { HttpServiceService } from './services/http-service.service';
 
 const appRoutes: Routes = [
